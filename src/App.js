14bcdbc919e4881023aba5e5be25cf1a0d2c76c2
silviamanoliu <manoliusilvia2002@gmail.com>
2024-08-13 11:45:03 +0300

    useContext update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootPage from "./pages/Root";
+import ErrorPage from "./pages/ErrorPage";
+import UsersPage from "./pages/Users";
+import AuthForm, {signinAction, signupAction} from "./components/AuthForm";
+import UserProfilePage, {profileLoader} from "./pages/UserProfile";
+import React, {useContext} from "react";
+import AuthContextProvider, {AuthContext} from "./store/auth-context";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const ctx = useContext(AuthContext);
+
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <RootPage/>,
+            id: 'root',
+            errorElement: <ErrorPage/>,
+            children: [
+                {
+                    index: true,
+                    element: <UsersPage/>,
+                },
+                {
+                    path: '/signup',
+                    element: <AuthForm mode='signup'/>,
+                    action: ({request}) =>signupAction(request, ctx)
+                },
+                {
+                    path: '/signin',
+                    element: <AuthForm mode='signin'/>,
+                    action: ({request}) => signinAction(request, ctx)
+                },
+                {
+                    path: '/profile',
+                    element: <UserProfilePage/>,
+                    id: 'profile',
+                    loader: () => profileLoader(ctx)
+                }
+            ]
+        },
+    ])
+
+    return (
+        <AuthContextProvider>
+            <RouterProvider router={router}/>
+        </AuthContextProvider>
+    )
 }
 
 export default App;
